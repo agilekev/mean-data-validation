dc054d54b0cad664ddf79aa2b38e7cfd3858dd24
Kevin <agilekev@gmail.com>
2016-06-16 16:20:23 +0100

    first list from mongodb onto example_properties view

--- models/property.js
+++ models/property.js
@@ -1,7 +1,11 @@
 /**
  * Created by kevin on 16/06/2016.
  */
+
 var mongoose = require( 'mongoose' );
+mongoose.connect( 'mongodb://localhost/test' );
+
+
 var Schema   = mongoose.Schema;
 
 var properties = new Schema({
@@ -9,5 +13,6 @@ var properties = new Schema({
     propertyuuid    : String
 });
 
-mongoose.model( 'properties', properties );
-mongoose.connect( 'mongodb://localhost/test' );
+var propertyData = mongoose.model( 'properties', properties );
+
+module.exports = propertyData;
--- routes/example_properties.js
+++ routes/example_properties.js
@@ -1,19 +1,14 @@
+var propertyData = require('../models/property');
 var express = require('express');
 var router = express.Router();
-var mongoose = require( 'mongoose' );
 
 
 /* GET example_properties page. */
 router.get('/', function ( req, res ){
-    propertyData.find( function ( err, propertyData, count ){
-        res.render( 'example_properties', {
-            title : 'Example Properties',
-            propertyData : propertyData
-        });
+    propertyData.find({}, function(err, propertyData) {
+        res.render('example_properties', {propertyData: propertyData});
     });
 });
-
-
 
 module.exports = router;
 
