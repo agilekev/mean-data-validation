66ea0a6f04b01f768f532d5bf811eecefde232f1
Kevin <agilekev@gmail.com>
2016-06-16 12:42:34 +0100

    First commit

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var fs = require("fs");
+var router = express.Router();
+
+var propertydata = (function () {
+
+  var filecontents = fs.readFileSync("./public/json/shard.json");
+  return JSON.parse(filecontents);
+
+})();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', {propertydata : propertydata});
+});
+
+
+module.exports = router;
+
